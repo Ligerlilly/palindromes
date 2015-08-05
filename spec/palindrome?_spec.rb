0a606abf58a4f42aa18e013986c807dd26d37337
Jason Lane & Justin Scott <justin_and_jason@fake.com>
2015-08-05 15:51:05 -0700

    Add test to see if method returns true if input is not palindrome

--- spec/palindrome?_spec.rb
+++ spec/palindrome?_spec.rb
@@ -6,4 +6,7 @@ describe 'palindrome?' do
     expect(palindrome?('noon')).to eq(true)
   end
 
+  it 'returns false if input string is not a palindrome' do
+    expect(palindrome?('rock')).to eq(false)
+  end
 end
