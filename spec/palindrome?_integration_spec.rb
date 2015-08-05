c304567fb990cd34965e4257765140c3a57e7254
Jason Lane & Justin Scott <justin_and_jason@fake.com>
2015-08-05 16:05:54 -0700

    Add palindrome integration test

---
+++ spec/palindrome?_integration_spec.rb
@@ -0,0 +1,13 @@
+require('capybara/rspec')
+require("./app")
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe 'the palindrome path', {:type => :feature} do
+  it('takes user entry and returns true if it is a palindrome') do
+    visit '/'
+    fill_in('word', :with => 'noon')
+    click_button('Send')
+    expect(page).to have_content("It is a palindrome!")
+  end
+end
